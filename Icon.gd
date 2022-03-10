cbca871d9768e019d6422c3e7474eeea58ee6e3c
rakkarage <rakkarage@gmail.com>
2022-03-09 22:32:07 -0500

    x

--- Icon.gd
+++ Icon.gd
@@ -1,13 +1,9 @@
 extends Sprite
 
-const _end := 1024 - 32
-const _delay := 1
-const _time := 0.333
-
 func _ready():
 	var t := Tween.new();
 	add_child(t)
 	t.interpolate_property(self, "position",
-		position, Vector2(position.x + 200, position.y), _time,
-		Tween.EASE_OUT, Tween.TRANS_SPRING, _delay)
+		position, Vector2(position.x + 200, position.y), 3.333,
+		Tween.EASE_OUT, Tween.TRANS_SPRING, 1)
 	t.start()
