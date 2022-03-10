6dfe595ce47c9b76134e1430232302e2fa89c1cd
rakkarage <rakkarage@gmail.com>
2022-03-10 14:19:50 -0500

    x

--- Icon.gd
+++ Icon.gd
@@ -1,9 +1,13 @@
-extends Sprite
+extends Node
 
-func _ready():
+func _ready() -> void:
+	_go($Linear, Tween.TRANS_LINEAR)
+	_go($Spring, Tween.TRANS_SPRING)
+
+func _go(c: CanvasItem, tt) -> void:
 	var t := Tween.new();
 	add_child(t)
-	t.interpolate_property(self, "position",
-		position, Vector2(position.x + 200, position.y), 3.333,
-		Tween.TRANS_SPRING, Tween.EASE_OUT, 1)
+	t.interpolate_property(c, "position",
+		null, Vector2(c.position.x + 200, c.position.y),
+		3.333, tt, Tween.EASE_OUT, 1)
 	t.start()
