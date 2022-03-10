cfe92427898d023f754c009b5928b7a6916b774b
rakkarage <rakkarage@gmail.com>
2022-03-10 15:40:19 -0500

    4

--- Icon.gd
+++ Icon.gd
@@ -5,9 +5,9 @@ func _ready() -> void:
 	_go($Spring, Tween.TRANS_SPRING)
 
 func _go(c: CanvasItem, tt) -> void:
-	var t := Tween.new();
-	add_child(t)
-	t.interpolate_property(c, "position",
-		null, Vector2(c.position.x + 200, c.position.y),
-		3.333, tt, Tween.EASE_OUT, 1)
-	t.start()
+	var t := create_tween()
+	t.tween_interval(1)
+	t.set_trans(tt).set_ease(Tween.EASE_OUT)
+	t.tween_property(c, "position",
+		Vector2(c.position.x + 200, c.position.y),
+		3.333)
