790b635ed30ee2eed14bdffa911ac9cceb410267
rakkarage <rakkarage@gmail.com>
2022-03-09 22:44:44 -0500

    x

--- Icon.gd
+++ Icon.gd
@@ -5,5 +5,5 @@ func _ready():
 	add_child(t)
 	t.interpolate_property(self, "position",
 		position, Vector2(position.x + 200, position.y), 3.333,
-		Tween.EASE_OUT, Tween.TRANS_SPRING, 1)
+		Tween.TRANS_SPRING, Tween.EASE_OUT, 1)
 	t.start()
